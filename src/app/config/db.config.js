3c682c02391c1c0b91b20e18ed231684be8756c8
Luis A. Mendoza <lmendoza@sandiego.com.gt>
2021-09-22 19:16:40 -0600

    cambio base de datos
    se realiza un cambio a la base de datos para que se conecte en linea

--- src/app/config/db.config.js
+++ src/app/config/db.config.js
@@ -1,4 +1,17 @@
 module.exports={
+    HOST:"ec2-44-194-6-121.compute-1.amazonaws.com",
+    USER:"hgxshzhmjkdrfv",
+    PASSWORD:"cba435b77e272dbbb958c1b05c254b1fd3b5903fb2636ab6fc593b15167b5d7c",
+    DB: "d37hs92uiui5fp",
+    dialect: "postgres",
+    pool: {
+        max:10,
+        min:0,
+        acquire:30000,
+        idle:10000
+    }
+    /*
+    //Local
     HOST:"localhost",
     USER:"postgres",
     PASSWORD:"1234",
@@ -9,5 +22,5 @@ module.exports={
         min:0,
         acquire:30000,
         idle:10000
-    }
+    }*/
 };
